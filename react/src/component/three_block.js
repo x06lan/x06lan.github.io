00b7a74f4f9615f55f4123107e6d85d419202850
x06lan <x065p4mp6@gmail.com>
2022-01-29 07:41:01 +0800

    camera view

--- react/src/component/three_block.js
+++ react/src/component/three_block.js
@@ -1,27 +1,44 @@
 import * as THREE from 'three';
-import { TrackballControls } from './jsm/controls/TrackballControls.js';
+// import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js"
 let camera, scene, renderer;
 let geometry, material, mesh;
 let block_height=400
-let controls;
+let mouse={x:0,y:0}
 
 
 function animation( time ) {
 
 	// mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
+	// mesh.rotation.y = time / 1000;
+	camera.position.x=mouse.x/200
+	camera.position.y=mouse.y/200
+	
+	camera.lookAt( mesh.position );
+	// controls.update();
 	// mesh.position.x += 0.01
 	renderer.render( scene, camera );
 
 }
+function onDocumentMouseMove( event ) {
+
+	const windowHalfX = window.innerWidth / 2;
+	const windowHalfY = window.innerHeight / 2;
+	mouse.x = ( event.clientX - windowHalfX ) / 2;
+	mouse.y = ( event.clientY - windowHalfY ) / 2;
+	// console.log(mouse)
+
+}
 function init(targe) {
   	window.addEventListener('resize', onWindowResize);
+	document.addEventListener( 'mousemove', onDocumentMouseMove );
+	document.addEventListener( 'mouseover', onDocumentMouseMove );
 
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / block_height, 0.01, 10 );
 	camera.position.z = 1;
+	camera.position.x= 0.01
+	camera.position.y =0.01;
 
-	controls = new TrackballControls( camera, renderer.domElement );
+	// controls = new TrackballControls( camera, renderer.domElement );
 
 	scene = new THREE.Scene();
 	//background color
