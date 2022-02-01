7dc3951760bfdfafaa0632d20c45423cfd001673
x06lan <x065p4mp6@gmail.com>
2022-02-01 23:55:30 +0800

    finsh three headline

---
+++ react/build/precache-manifest.376731f81f2ec11f919057b6054d5c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcef0acf83c6240b8ce2b1664ec5743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "102f58b8c1efb4d503c3",
+    "url": "/static/css/main.ec4c0e48.chunk.css"
+  },
+  {
+    "revision": "08bc7122a77d9f336942",
+    "url": "/static/js/2.e58167b0.chunk.js"
+  },
+  {
+    "revision": "7da80a4b2c1fc8dd018d",
+    "url": "/static/js/3.e3b4af8e.chunk.js"
+  },
+  {
+    "revision": "102f58b8c1efb4d503c3",
+    "url": "/static/js/main.bffb8f6a.chunk.js"
+  },
+  {
+    "revision": "b22b5f536f8126263b01",
+    "url": "/static/js/runtime~main.1b1cf5ad.js"
+  }
+]);
--- react/src/component/three_block.js
+++ react/src/component/three_block.js
@@ -1,61 +1,52 @@
 import * as THREE from 'three';
-import {create,all, add, pi}from 'mathjs'
+import {create,all, add, pi, index}from 'mathjs'
 import { MeshStandardMaterial } from 'three';
+import { Perlin } from 'three-noise';
 import { seededRandom } from 'three/src/math/MathUtils';
 const math = create(all)
-// import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js"
+let noise = new Perlin(math.random());
 let camera, scene, renderer;
-let geometry, material, mesh;
+let geometry ,material ,meshs;
 let block_height=400
 let mouse={x:0,y:0}
-let step_ff=1
 let cam_move={x:0,y:0,z:0}
+let cubes=[]
+let size=10
+let gap=0.2
 
-// let time=0
 function animation( time ) {
 
-	// let count=time/1000
-	let dt=0.05
-	
-	let add_v=math.pi/100
+	let dt=0.0005
+	// pos.x=time*dt
+	// console.log(noise.get3(pos))
 	camera.position.x=mouse.x
-	camera.position.y=mouse.y
-	// // console.log(step_ff)
-	// // console.log(mesh.rotation)
-	// if (step_ff==0){
-	// 	// console.log("z")
-	// 	mesh.position.z+=add_v
-	// 	camera.position.z=mesh.position.z+3
-	// 	if (mesh.position.z%(1)<dt){
-	// 		step_ff=1
-	// 	}
-	// }
-	// if (step_ff==1){
-	// 	// console.log("y")
-	// 	mesh.position.x+=add_v
-	// 	camera.position.x=mesh.position.x+1
-	// 	if (mesh.position.x%(1)<dt){
-	// 		step_ff=0
-	// 	}
-	// }
-	// if (step_ff==2){ 
-	// 	console.log("x")
-	// 	mesh.rotation.y+=add_v
-	// 	if (mesh.rotation.y%(math.pi/2)<dt){
-	// 		step_ff=0
-	// 	}
-	// }
-	// time+=math.pi/300
-	// console.log(math.sin(time/1000))
-	// console.log(time)
-	// mesh.rotation.y=math.pi/4
-	// mesh.rotation.x+=temp/10
-	// mesh.rotation.z+=temp/10
+	camera.position.y=mouse.y+3
 	
-	// camera.position.x=+mouse.x
-	// camera.position.y=mouse.y+1
-	
-	camera.lookAt( mesh.position );
+	let gap_noise=0.7
+	// console.log(size)
+	meshs.rotation.y=dt*time
+	for (let index_x= 0; index_x <size; index_x++) {
+		for(let index_y=0; index_y<size;index_y++){
+			for(let index_z=0; index_z<size;index_z++){
+				let cube=cubes[index_x][index_y][index_z]
+				const pos = new THREE.Vector3()
+				pos.x=index_x*gap*gap_noise+time*dt
+				pos.y=index_y*gap*gap_noise+time*dt
+				pos.z=index_z*gap*gap_noise+time*dt
+				let new_size=noise.get3(pos)*1.3+1
+				cube.scale.x=new_size
+				cube.scale.y=new_size
+				cube.scale.z=new_size
+				
+
+			}
+		}
+	}
+	if(time<10)	{
+
+	// console.log(cubes)
+	}
+	camera.lookAt( meshs.position );
 	renderer.render( scene, camera );
 
 }
@@ -65,7 +56,6 @@ function onDocumentMouseMove( event ) {
 	const windowHalfY = window.innerHeight / 2;
 	mouse.x = ( event.clientX - windowHalfX ) /windowHalfX/2;
 	mouse.y = ( event.clientY - windowHalfY ) / windowHalfY/2;
-	// console.log(mouse)
 
 }
 function init(targe) {
@@ -76,29 +66,50 @@ function init(targe) {
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / block_height, 0.01, 10 );
 	camera.position.z = 3;
 	camera.position.x= 1;
-	camera.position.y =1;
-
-	// controls = new TrackballControls( camera, renderer.domElement );
 
 	scene = new THREE.Scene();
 	//background color
 	scene.background = new THREE.Color( 0x3cffbe);
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	// geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
 	material = new THREE.MeshNormalMaterial();
+	geometry = new THREE.BufferGeometry();
 
-	mesh = new THREE.Mesh( geometry, material );
-	// mesh.position.y=-1
-	scene.add( mesh );
+	let group= new THREE.Mesh( geometry, material );
+	meshs = new THREE.Mesh( geometry, material );
+	for (let index_x= 1; index_x <= size; index_x++) {
+		let tem2=[]
+		for(let index_y=1; index_y<=size;index_y++){
 
-	console.log(mesh)
+			let tem=[]
+			for(let index_z=1; index_z<=size;index_z++){
+				let tem_geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2);
+				let tem_material = new THREE.MeshNormalMaterial();
+				let tem_mesh = new THREE.Mesh( tem_geometry, tem_material );
+				tem_mesh.position.x=index_x*gap-size/2*gap
+				tem_mesh.position.y=index_y*gap-size/2*gap
+				tem_mesh.position.z=index_z*gap-size/2*gap
+				group.add(tem_mesh)
+				tem.push(tem_mesh)
+			}
+			tem2.push(tem)
+		}
+		cubes.push(tem2)
+	}
+
+	group.rotation.x=math.pi/4
+	group.rotation.z=math.pi/4
+	
+
+	meshs.add(group)
+
+	scene.add( meshs );
+	console.log(cubes)
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, block_height);
 	renderer.setAnimationLoop( animation );
-	// let tem=
 	document.body.appendChild( renderer.domElement );
 	document.getElementById(targe).appendChild(renderer.domElement)
-	// return (renderer.domElement)
 	const grid = new THREE.GridHelper( 100, 100, 0x000000, 0x000000 );
 	
 	grid.material.opacity = 0.2; 
